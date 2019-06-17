645b6b095e117050f818ddf8b5a3448c8a7d6a8a
zhengbigbig <780357902@qq.com>
2019-06-17 16:34:25 +0800

    集合 List Set

--- src/demo6/Main.java
+++ src/demo6/Main.java
@@ -1,13 +1,12 @@
 package demo6;
 
-import java.util.HashMap;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         // 泛型
         // key是String ,value是Integer
-        HashMap<String,Integer> counts = new HashMap<String, Integer>();
+        HashMap<String, Integer> counts = new HashMap<String, Integer>();
         Stack<String> stringStack = new Stack<String>();
         Stack<Integer> intStack = new Stack<Integer>();
 
@@ -16,5 +15,31 @@ public class Main {
 
         intStack.push(12);
         Integer two = intStack.pop();
+
+        // 集合
+
+        // 列表
+        List<String> arrayList = new ArrayList<>();
+        List<String> linkedList = new LinkedList<>();
+
+        arrayList.add("a");
+//        arrayList.get(0);
+//        arrayList.subList(0, 5); // 从位置0开始，到5的子列表
+
+        // for 循环遍历列表
+        for (String item : arrayList) {
+            System.out.println(item);
+        }
+
+        // Set -- 主要用于去重
+        Set<String> set = new HashSet<>();
+
+        set.add("x");
+        set.add("y");
+        set.add("x");
+
+        for(String item: set){
+            System.out.println(item);
+        }
     }
 }
